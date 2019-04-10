c22ceb1b09b20b86ae54685b744579169ba02bc2
s21600337 <21600337@gmail.com>
2019-04-10 11:48:54 +0900

    user.c

--- user.c
+++ user.c
@@ -1,25 +1,47 @@
 #include "user.h"
 
+void showList(LOGIN* list[],int count){
+ int number=0;
+ for(int i=0; i<count; i++){
+   printf("%s %s\n",list[number]->id, list[number]->password);
+   number++;
+ }
+#ifdef DEBUG_MODE
+  printf("DEBUG>> showList\n");
+#endif  
+}
+
 int load_file(LOGIN* list[], char* filename){
   int count=0;
+  char check;
   FILE *datafile = fopen(filename, "r");
   while(!feof(datafile)){
     list[count]=(LOGIN*)malloc(sizeof(LOGIN));
     fscanf(datafile,"%s %s",list[count]->id,list[count]->password);
     count++;
   }
-  printf("%d records read!\n",count);\
-  fclose(datafile);
+    count--;
+ if(count==0){
+   printf("Error no data \n");
+ }else{
+   printf("%d records read!\n",count);\
+ }
+ fclose(datafile);
+#ifdef DEBUG_MODE
+  printf("DEBUG>> showList\n");
+#endif
+
   return count;
 }
 
-void join(LOGIN* list[], int count){
+void join(LOGIN* list[], int* count,char* filename){
   char id[20], pass[20];
+  FILE *datafile = fopen(filename, "a"); 
   while(1){
     printf("Enter new user id >> ");
     scanf("%s", id);
     int dup=0;
-    for(int i=0;i<count;i++){
+    for(int i=0;i<*count;i++){
       if(strcmp(id, list[i]->id)==0) {
         dup=1; break;
       }
@@ -30,13 +52,21 @@ void join(LOGIN* list[], int count){
     else{
       printf("Enter password >> ");
       scanf("%s", pass);
-      list[count] = (LOGIN*)malloc(sizeof(LOGIN));
-      strcpy(list[count]->id, id);
-      strcpy(list[count]->password, pass);
+      list[*count] = (LOGIN*)malloc(sizeof(LOGIN));
+      strcpy(list[*count]->id, id);
+        fprintf(datafile,"%s ",id );
+      strcpy(list[*count]->password, pass);
+        fprintf(datafile,"%s",pass );
+        fprintf(datafile,"\n");
+      *count=*count+1;
       printf("New user added!\n");
       break;
     }
   }
+#ifdef DEBUG_MODE
+  printf("DEBUG>> join\n");
+#endif
+
 }
 
 int login(LOGIN* list[], int count){
@@ -53,7 +83,7 @@ int login(LOGIN* list[], int count){
   }
   if(dup!=1){
     printf("No such user!!\n");
-    return -1;
+    return 0;
   }
   else{
     printf("Enter password >> ");
@@ -67,6 +97,10 @@ int login(LOGIN* list[], int count){
       return 0;
     }
   }
+#ifdef DEBUG_MODE
+  printf("DEBUG>> login\n");
+#endif
+
 }
 
 void logout(int* is_login){
@@ -81,4 +115,8 @@ void save_file(LOGIN* list[], int count, char* filename){
   }
   printf("%s Saved!\n", filename);
   fclose(datafile);
+#ifdef DEBUG_MODE
+  printf("DEBUG>> logout\n");
+#endif
+
 }
